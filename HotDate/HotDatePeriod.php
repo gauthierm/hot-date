537d9ed1a2cbbdc2277d3578382a296c8bdf1271
Michael Gauthier <mike@silverorange.com>
2010-09-15 14:16:13 +0000

    Skeleton of a DatePeriod class.
    git-svn-id: file:///Users/gauthierm/svnrepo/public/hot-date/trunk@57508 94490950-a86c-4493-9ccb-530f72933e47

--- HotDate/HotDatePeriod.php
+++ HotDate/HotDatePeriod.php
@@ -1,7 +1,71 @@
 <?php
 
-class HotDatePeriod
+class HotDatePeriod implements Iterator
 {
+	const EXCLUDE_START_DATE = 1;
+
+	public function __construct(
+		$param1,
+		$param2 = null,
+		$param3 = null,
+		$param4 = null
+	) {
+		if (is_string($param1)) {
+			if (is_int($param2)) {
+				$options = $param2;
+			}
+		} else {
+
+			if (!($param1 instanceof HotDateTime)) {
+				throw new InvalidArgumentException(
+					'$start must be an instance of HotDateTime.'
+				);
+			}
+			$start = $param1;
+
+			if (!($param2 instanceof HotDateInterval)) {
+				throw new InvalidArgumentException(
+					'$interval must be an instance of HotDateInterval.'
+				);
+			}
+			$interval = $param2;
+
+			if (is_int($param3)) {
+				$recurrences = $param3;
+			} else {
+				if (!($param3 instanceof HotDateTime)) {
+					throw new InvalidArgumentException(
+						'$end must be an instance of HotDateTime.'
+					);
+				}
+				$end = $param3;
+			}
+
+			if (is_int($param4)) {
+				$options = $param4;
+			}
+		}
+	}
+
+	public function current()
+	{
+	}
+
+	public function key()
+	{
+	}
+
+	public function next()
+	{
+	}
+
+	public function rewind()
+	{
+	}
+
+	public function valid()
+	{
+	}
 }
 
 ?>
