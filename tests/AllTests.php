747d28a5c6bc72602a931bd46948bb73d0ea0168
Michael Gauthier <mike@silverorange.com>
2010-09-15 14:15:47 +0000

    Start unit tests for HotDate package.
    git-svn-id: file:///Users/gauthierm/svnrepo/public/hot-date/trunk@57507 94490950-a86c-4493-9ccb-530f72933e47

---
+++ tests/AllTests.php
@@ -0,0 +1,37 @@
+<?php
+
+if (!defined('PHPUnit_MAIN_METHOD')) {
+    define('PHPUnit_MAIN_METHOD', 'HotDate_AllTests::main');
+}
+
+require_once 'PHPUnit/TextUI/TestRunner.php';
+
+require_once 'HotDateTimeDiffTest.php';
+
+class HotDate_AllTests
+{
+    // {{{ main()
+
+    public static function main()
+    {
+        PHPUnit_TextUI_TestRunner::run(self::suite());
+    }
+
+    // }}}
+    // {{{ suite()
+
+    public static function suite()
+    {
+        $suite = new PHPUnit_Framework_TestSuite('HotDate Tests');
+        $suite->addTestSuite('HotDateTimeDiffTest');
+        return $suite;
+    }
+
+    // }}}
+}
+
+if (PHPUnit_MAIN_METHOD == 'HotDate_AllTests::main') {
+    HotDate_AllTests::main();
+}
+
+?>
