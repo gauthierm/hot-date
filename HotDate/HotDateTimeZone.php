93cb4de360aae30be979f8cd468537715d3e1096
Michael Gauthier <mike@silverorange.com>
2010-09-17 16:00:57 +0000

    Fix time-zone region constants and fix bugs in listIdentifiers(). The listIndentifiers() method now filters out invalid time zones like 'Etc/GMT+9' and 'US/Alaska'.
    svn commit r57542

--- HotDate/HotDateTimeZone.php
+++ HotDate/HotDateTimeZone.php
@@ -5,7 +5,7 @@ class HotDateTimeZone extends DateTimeZone
 	const AFRICA      = 1;
 	const AMERICA     = 2;
 	const ANTARCTICA  = 4;
-	const ARTIC       = 8;
+	const ARCTIC      = 8;
 	const ASIA        = 16;
 	const ATLANTIC    = 32;
 	const AUSTRALIA   = 64;
@@ -23,14 +23,14 @@ class HotDateTimeZone extends DateTimeZone
 		HotDateTimeZone::AFRICA     => 'Africa',
 		HotDateTimeZone::AMERICA    => 'America',
 		HotDateTimeZone::ANTARCTICA => 'Antarctica',
-		HotDateTimeZone::ARTIC      => 'Artic',
+		HotDateTimeZone::ARCTIC     => 'Arctic',
 		HotDateTimeZone::ASIA       => 'Asia',
-		HotDateTimeZone::ASIA       => 'Atlantic',
-		HotDateTimeZone::ASIA       => 'Australia',
-		HotDateTimeZone::ASIA       => 'Europe',
-		HotDateTimeZone::ASIA       => 'Indian',
-		HotDateTimeZone::ASIA       => 'Pacific',
-		HotDateTimeZone::ASIA       => 'UTC',
+		HotDateTimeZone::ATLANTIC   => 'Atlantic',
+		HotDateTimeZone::AUSTRALIA  => 'Australia',
+		HotDateTimeZone::EUROPE     => 'Europe',
+		HotDateTimeZone::INDIAN     => 'Indian',
+		HotDateTimeZone::PACIFIC    => 'Pacific',
+		HotDateTimeZone::UTC        => 'UTC',
 	);
 
 	public function getLocation()
@@ -61,16 +61,14 @@ class HotDateTimeZone extends DateTimeZone
 	}
 
 	public static function listIdentifiers(
-		$what = DateTimeZone::ALL,
+		$what = HotDateTimeZone::ALL,
 		$country = null
 	) {
 		if (self::$identifiers === null) {
 			self::$identifiers = DateTimeZone::listIdentifiers();
 		}
 
-		if ($what === HotDateTimeZone::ALL) {
-			$identifiers = self::$identifiers;
-		} elseif ($what === HotDateTimeZone::PER_COUNTRY) {
+		if ($what === HotDateTimeZone::PER_COUNTRY) {
 			// unsupported
 		} else {
 			if (!isset(self::$identifiersByWhat[$what])) {
@@ -78,7 +76,7 @@ class HotDateTimeZone extends DateTimeZone
 				// build requested regions array
 				$regions = array();
 				foreach (self::$zoneMap as $key => $zone) {
-					if ($key & $what === $key) {
+					if (($what & $key) === $key) {
 						$regions[$zone] = $zone;
 					}
 				}
@@ -86,8 +84,7 @@ class HotDateTimeZone extends DateTimeZone
 				// build identifiers for requested regions
 				self::$identifiersByWhat[$what] = array();
 				foreach (self::$identifiers as $identifier) {
-					$parts = explode('/', $identifier, 2);
-					$zone  = $parts[0];
+					$zone = reset(explode('/', $identifier, 2));
 					if (isset($regions[$zone])) {
 						self::$identifiersByWhat[$what][] = $identifier;
 					}
