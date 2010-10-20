2d763d91e18242a5fe673b1d3cb94f58e62764e8
Keith Burgoyne <keith@silverorange.com>
2010-10-20 15:04:25 +0000

    prepare for release of 0.1.1
    git-svn-id: file:///Users/gauthierm/svnrepo/public/hot-date/trunk@58164 94490950-a86c-4493-9ccb-530f72933e47

---
+++ package.php
@@ -0,0 +1,59 @@
+<?php
+
+require_once 'PEAR/PackageFileManager2.php';
+
+$version = '0.1.1';
+$notes = <<<EOT
+see ChangeLog
+EOT;
+
+$description =<<<EOT
+Rawk!
+EOT;
+
+$package = new PEAR_PackageFileManager2();
+PEAR::setErrorHandling(PEAR_ERROR_DIE);
+
+$result = $package->setOptions(
+	array(
+		'filelistgenerator' => 'svn',
+		'simpleoutput'      => true,
+		'baseinstalldir'    => '/',
+		'packagedirectory'  => './',
+		'dir_roles'         => array(
+			'HotDate'      => 'php',
+			'locale'       => 'data',
+			'www'          => 'data',
+			'dependencies' => 'data',
+		),
+	)
+);
+
+$package->setPackage('HotDate');
+$package->setSummary('Hot Date!');
+$package->setDescription($description);
+$package->setChannel('pear.silverorange.com');
+$package->setPackageType('php');
+$package->setLicense('private', 'http://www.silverorange.com/');
+
+$package->setReleaseVersion($version);
+$package->setReleaseStability('alpha');
+$package->setAPIVersion('0.1.1');
+$package->setAPIStability('alpha');
+$package->setNotes($notes);
+
+$package->addIgnore('package.php');
+
+$package->addMaintainer('lead', 'gauthierm', 'Mike Gauthier', 'mike@silverorange.com');
+
+$package->setPhpDep('5.2.0');
+$package->setPearinstallerDep('1.4.0');
+$package->generateContents();
+
+if (isset($_GET['make']) || (isset($_SERVER['argv']) && @$_SERVER['argv'][1] == 'make')) {
+	$package->writePackageFile();
+} else {
+	$package->debugPackageFile();
+}
+
+?>
